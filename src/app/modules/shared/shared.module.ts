d7552fadbdbd3034b6bbfc83a41ef4010be202bb
Janusz Sokołow <jsokolow@efigence.com>
2022-08-20 00:08:15 +0200

    Init nav & design structure

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, NavComponent],
   imports: [CommonModule],
-  exports: [FooterComponent],
+  exports: [FooterComponent, NavComponent],
 })
 export class SharedModule {}
